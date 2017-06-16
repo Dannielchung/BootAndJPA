48955fcfae5d51dac6b70fdc01cdcf38a539feaf
蓝天翔 <tianxianglan@126.com>
2017-06-17 02:04:17 +0800

    完善全局异常的处理

--- src/main/java/com/example/demo/controller/ExceptionController.java
+++ src/main/java/com/example/demo/controller/ExceptionController.java
@@ -4,6 +4,7 @@ import com.example.demo.common.Response;
 import com.example.demo.exception.InvalidException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,17 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by tianxianglan on 2017/6/17.
  */
 @RestController
+@ControllerAdvice
 public class ExceptionController {
 
     private static Logger LOGGER = LoggerFactory.getLogger(ExceptionController.class);
 
-    @ExceptionHandler
+    @ExceptionHandler(value = InvalidException.class)
     public Response doException(Exception e){
-        if (e instanceof InvalidException){
-            LOGGER.error("无效操作!");
-            return new Response(e.getMessage());
-        }
-        return new Response();
+        LOGGER.error("无效操作!");
+        return new Response(e.getMessage());
     }
 
 }
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,12 +7,14 @@ import com.example.demo.entity.User;
 import com.example.demo.service.Impl.UserDaoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by tianxianglan on 2017/5/30.
  */
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     UserDaoImpl userDao;
